9e20d8e4a8b59d9ffa9ba8649e5fa6fd3bfe1256
Fachii Zasha <zashafachii@gmail.com>
2022-01-04 05:18:55 +0100

    Create an Order data model.
    1. Create an Order class in the store/models.py.
    2. Add a membership field to the Customer data model.
    3. Add comments.

--- store/models.py
+++ store/models.py
@@ -1,7 +1,20 @@
+    """
+    This module provides the function to create Store Data Model which comprises of a Product, Customer and Order object.
+    """
 from django.db import models
 
 
 class Product(models.Model):
+    """
+    A Product class for creating a Product obj.
+
+    Fields:
+    title (str): The field column for adding the title of a product.
+    description (str): The field column for adding the description of a product.
+    price (int): The field column for adding the price of a product.
+    inventory (int): The field column for adding the number of the product available.
+    last_update (int): The field column for adding the date and time at which a product is updated.
+    """
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -10,8 +23,49 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
+    """
+    A Customer class for creating a Customer obj.
+
+    Fields:
+    first_name (str): The field column for adding the first name of a customer.
+    last_name (str): The field column for adding the last name of a customer.
+    email (str): The field column for adding the email address of a customer (no duplicates).
+    phone (str): The field column for adding the phone number of a customer.
+    birthday (int): The field column for adding the birth day of a customer (optional).
+    membership (str): The field column for adding the rank of a customer.
+    """
+    MEMBERSHIP_BRONZE = "B"
+    MEMBERSHIP_SILVER = "S"
+    MEMBERSHIP_GOLD = "G"
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=254, unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=6, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    
+
+class Order(models.Model):
+    """
+    A Order class for creating an order obj.
+
+    Fields:
+    placed_at (int): The field column for adding the date and time at which an order is placed.
+    payment_status (int): The field column for adding the status of the payment
+    
+    """
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=255, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
