4d3c39334876f76b67a53f447bee3c301fb8e097
Fachii Zasha <zashafachii@gmail.com>
2022-01-09 19:47:34 +0100

    Create urls.py module in the playground app package and a templates folder in the base directory.
    1. Create urls.py module to map incoming request via route to the playground app package.
    2. Create and implement a home.html in the templates folder.
    3. Update the business logic of the playground/views.py module.
    4. Update TEMPLATES settings in the settings.py module to attach/read templates folder when looking for templates.
    5. Direct the route in the storefront/urls to the playground/urls.py module.

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,12 @@
+from django.db.models import query
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.db.models import Q
 
-# Create your views here.
+from store.models import Product
+
+
+def index(request):
+    queryset = Product.objects.filter(Q(inventory__gt=10) | Q(unit_price__lt=20))
+    
+    return render(request, "home.html", {"name": "Fachiis", "products": list(queryset)})
--- storefront/urls.py
+++ storefront/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("playground.urls")),
     path("__debug__", include("debug_toolbar.urls")),
 ]
