d2bda94be278dd932d3bd8a4f5a56dc1532edede
Fachii Zasha <zashafachii@gmail.com>
2022-01-11 03:06:35 +0100

    Create updates.
    1. Return a render function in the playground/views.py module for displaying the template.
    2. Create a new objects manager, TaggedItemManager, in the tags/models.py module.
    3. Change the display effect.

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,5 @@
-from django.db.models import query
-from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.db.models import Q
-
-from store.models import Product
 
 
-def index(request):
-    queryset = Product.objects.filter(Q(inventory__gt=10) | Q(unit_price__lt=20))
-    
-    return render(request, "home.html", {"name": "Fachiis", "products": list(queryset)})
+def index(request): 
+    return render(request, "home.html", {"name": "Fachiis"})
--- tags/models.py
+++ tags/models.py
@@ -6,6 +6,19 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 
+class TaggedItemManager(models.Manager):
+    """
+    A TaggedItemManager for creating a clean method for calling a object type and the object id.
+    """
+
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        return TaggedItem.objects.select_related("tag").filter(
+            content_type=content_type, object_id=obj_id
+        )
+
+
 class Tag(models.Model):
     """
     A Tag class for creating a tag obj.
@@ -13,8 +26,9 @@ class Tag(models.Model):
     Fields:
         label (str): The field for adding the label of a tag.
     """
+
     label = models.CharField(max_length=255)
-    
+
 
 class TaggedItem(models.Model):
     """
@@ -24,12 +38,13 @@ class TaggedItem(models.Model):
         tag (str): The field for connecting many tagitems to a tag.
         Generic Relation Creation...
     """
+
+    objects = TaggedItemManager()
     tag = models.ForeignKey(to=Tag, on_delete=models.CASCADE)
     # what object to tag (e.g product, article, video)
     # TYPE (Product, Article, Video)
     # ID (a particular row of interest)
-     # content_obj (reading that obj)
+    # content_obj (reading that obj)
     content_type = models.ForeignKey(to=ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
-    
