83f46866412f8863f471c31177d87d1b0b3b9754
fachiis <zashafachii@gmail.com>
2022-05-11 01:59:17 +0100

    Created custom django command to add data to database using an sql file and Modify root url have "store".

--- storefront/urls.py
+++ storefront/urls.py
@@ -21,7 +21,7 @@ admin.site.index_title = "Admin"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include("store.urls")),
+    path("api/v1/store/", include("store.urls")),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
     path("__debug__", include("debug_toolbar.urls")),
